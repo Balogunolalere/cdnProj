5f85ab5fc0992a00ec628abfae03f34ac630566f
Balogunolalere <lordareello@gmail.com>
2023-08-08 20:13:40 +0100

    changes

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 from fastapi import FastAPI, UploadFile, HTTPException
 from fastapi.responses import HTMLResponse
 from fastapi.responses import Response
-from fastapi.middleware.gzip import GZipMiddleware
+
 import  deta 
 import logging
 import os
@@ -24,12 +24,6 @@ app = FastAPI(
 
 # Connect to Deta Drive
 drive = deta.Deta(DRIVE_PROJECT_KEY).Drive(DRIVE_PROJECT_NAME)
-
-
-# Add GZipMiddleware to compress response data
-app.add_middleware(GZipMiddleware, minimum_size=1000)
-
-
 
 # Set maximum file size to 100 MB
 MAX_FILE_SIZE = 100 * 1024 * 1024
@@ -221,4 +215,3 @@ async def root():
     </html>
     """
     return HTMLResponse(content=html_content, status_code=200)
-
